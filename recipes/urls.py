5c9e8397f46fb1d5c57bc8d46a565684577d4e41
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-05-07 13:13:40 -0300

    Updating urls.py and views.py from recipes app

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('recipe/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render  # type: ignore
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request):
+    return render(request, 'recipes/pages/recipe-view.html')
