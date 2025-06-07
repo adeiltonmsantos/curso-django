66214929b893149ece6852896f5dc7b0d3c4e38d
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-06-07 09:12:37 -0300

    Creating /utils/recipe folder for fatory.py script (data recipes generator)

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render  # type: ignore
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={})
 
 
 def recipe(request):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={})
