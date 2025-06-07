c7e60ff5c43ef8958db2e260328bcdf560d7e5d8
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-06-07 15:34:08 -0300

    Injecting data with /utils/recipes/factory.py in home.html and recipe.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,22 @@
 # from django.shortcuts import render
 from django.shortcuts import render  # type: ignore
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={})
+    return render(
+        request,
+        'recipes/pages/home.html',
+        context={
+            'recipes': [make_recipe() for _ in range(10)]
+        })
 
 
 def recipe(request):
-    return render(request, 'recipes/pages/recipe-view.html', context={})
+    return render(
+        request,
+        'recipes/pages/recipe-view.html',
+        context={
+            'recipe': make_recipe()
+        })
