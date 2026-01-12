b7375cf3bb45cd63038ab2ecaede196950a15540
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-01-12 13:53:43 -0300

    Using shortcut in view.py and adjusting recipe.html to show HTML (preparation steps)

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,5 @@
 # from django.shortcuts import render
-from django.shortcuts import render  # type: ignore
-
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, render  # type: ignore
 
 from .models import Recipe
 
@@ -17,21 +15,37 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('title')
-    return render(
-        request,
-        'recipes/pages/category.html',
-        context={
-            'recipes': recipes
-        })
+    rcps = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+
+    recipes = get_list_or_404(rcps)
+
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True,
+    # ).order_by('-id')
+
+    # if not recipes:
+    #     raise Http404('Not found 🥲')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(
         request,
         'recipes/pages/recipe-view.html',
         context={
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail_page': True,
         })
