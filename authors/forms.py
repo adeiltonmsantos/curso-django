3760927c7286d279ae4f89de3d1e5d492ea09325
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-02-04 22:38:08 -0300

    Updating labels and help_texts in user form in forms.py

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,13 @@ class RegisterForm(ModelForm):
             'email',
             'password'
         ]
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'first_name': 'O nome deve ter pelo menos 4 caracteres'
+        }
