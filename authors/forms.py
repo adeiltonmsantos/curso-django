c0bb4f7367d9ca6b71999c74639b2a92e4ffd8a0
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-02-06 22:29:59 -0300

    Overriding the Form constructor to automatically set field attributes

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,44 @@
+from django import forms
 from django.contrib.auth.models import User
-from django.forms import ModelForm
 
 
-class RegisterForm(ModelForm):
+def edit_attr_widget_field(field, attr_name, attr_val):
+    field.widget.attrs[attr_name] = attr_val
+
+
+class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        edit_attr_widget_field(
+            self.fields['username'], 'placeholder', 'Your username here')
+        edit_attr_widget_field(
+            self.fields['email'], 'placeholder', 'Your e-mail here')
+        edit_attr_widget_field(
+            self.fields['first_name'], 'placeholder', 'Ex.: John')
+        edit_attr_widget_field(
+            self.fields['last_name'], 'placeholder', 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -10,15 +46,30 @@ class RegisterForm(ModelForm):
             'last_name',
             'username',
             'email',
-            'password'
+            'password',
         ]
+        # exclude = ['first_name']
         labels = {
-            'first_name': 'First Name',
-            'last_name': 'Last Name',
             'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
             'email': 'E-mail',
             'password': 'Password',
         }
         help_texts = {
-            'first_name': 'O nome deve ter pelo menos 4 caracteres'
+            'email': 'The e-mail must be valid.',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+                'class': 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
         }
