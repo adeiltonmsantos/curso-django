8bf680f32089138238dce5d549663145cb13b6d7
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-02-19 16:50:05 -0300

    Just to register creation of folders: base-static and base-templates/global/partials

--- project/settings.py
+++ project/settings.py
@@ -132,6 +132,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Where `collectstatic` will collect static files for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'base-static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
