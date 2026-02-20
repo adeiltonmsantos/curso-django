05153de0f8a5becccfa0b52bfc9eb8c90ab34eb4
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-02-19 22:46:32 -0300

    Adding placeholder in login form fields

--- authors/forms/login_form.py
+++ authors/forms/login_form.py
@@ -1,6 +1,15 @@
 from django import forms
 
+from utils.django_forms import edit_attr_widget_field
+
 
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        edit_attr_widget_field(
+            self.fields['username'], 'placeholder', 'Type your username here')
+        edit_attr_widget_field(
+            self.fields['password'], 'placeholder', 'Type your password here')
+
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
