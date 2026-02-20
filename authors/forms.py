de2fb4d125347836a678a1dc9fcbacba6480c690
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-02-19 21:54:24 -0300

    Moving functions out of the context of the user registration form class

--- authors/forms.py
+++ authors/forms.py
@@ -1,40 +1,12 @@
-import re
-
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-
-def edit_attr_widget_field(field, attr_name, attr_val):
-    field.widget.attrs[attr_name] = attr_val
-
-
-def field_size_correct(field_name_print, field_cleaned_data, field_size):  # noqa: E501
-    """
-    field_size_correct(): if field size < 'field_size' a ValidationError is raised
-        - field_name_print: field name to show in message
-        - field_cleaned_data: value of field from self.cleaned_data
-        - field_size: if filed size is smaller then field_size an error raises # noqa: E501
-    """
-    if len(field_cleaned_data) < field_size:
-        raise ValidationError(
-            f'"{field_name_print}" field must have more then %(value)s letters',  # noqa: E501
-            code='invalid',
-            params={'value': field_size}
-        )
-
-
-def strong_password(password):
-    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
-
-    if not regex.match(password):
-        raise ValidationError((
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
-        ),
-            code='invalid'
-        )
+from utils.django_forms import (
+    edit_attr_widget_field,
+    field_size_correct,
+    strong_password,
+)
 
 
 class RegisterForm(forms.ModelForm):
