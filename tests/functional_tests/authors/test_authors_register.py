31e502c26a41c6422e3c8c9226bc2c7bc66d46d2
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-11 14:01:02 -0200

    Finishing first version of first_name field validation of user register form

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -11,6 +11,12 @@ class AuthorsRegisterFunctionalTest(AuthorsBaseFunctionalTest):
             f'//input[@placeholder="{placeholder}"]'
         )
 
+    def get_by_fullxpath(self, xpath):
+        return self.browser.find_element(
+            By.XPATH,
+            xpath
+        )
+
     def fill_form_dummy_data(self, form):
         fields = form.find_elements(
             By.TAG_NAME,
@@ -20,21 +26,32 @@ class AuthorsRegisterFunctionalTest(AuthorsBaseFunctionalTest):
             if field.is_displayed():
                 field.send_keys(' ' * 20)
 
-    def test_the_test(self):
+    def test_empty_first_name_error_message(self):
+        # Opening browser in form route
         self.browser.get(self.live_server_url + '/authors/register/')
 
-        form = self.browser.find_element(
-            By.XPATH,
-            '/html/body/main/div[3]/form'
-        )
+        # Selecting form in browser
+        form = self.get_by_fullxpath('/html/body/main/div[3]/form')
 
+        # Filling form fields
         self.fill_form_dummy_data(form)
+
+        # Selecting e-mail field to fill with valid characters
         form.find_element(
             By.NAME,
             'email'
         ).send_keys('dummy@email.com')
 
+        # Selecting desired field (first_name)
         first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+
+        # Leaving first_name empty to invalidate form
+        first_name_field.send_keys('    ')
+
+        # Submiting form
         first_name_field.send_keys(Keys.ENTER)
 
-        self.sleep()
+        # Selecting form in browser again after page reloading
+        form = self.get_by_fullxpath('/html/body/main/div[3]/form')
+
+        self.assertIn('First name must not be empty', form.text)
