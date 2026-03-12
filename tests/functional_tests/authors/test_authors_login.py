736d452bd629f098c0ce1e56eb5d60facdec30af
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-12 16:50:28 -0300

    Starting to define functional test for succesfull login

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -1,5 +1,41 @@
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from selenium.webdriver.common.by import By
+
 from .base import AuthorsBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class AuthorsLoginFunctionalTest(AuthorsBaseFunctionalTest):
-    pass
+    def test_user_valid_data_can_login_successfully(self):
+        passwd = 'abc123'
+        user = User.objects.create_user(
+            username='Username',
+            password=passwd
+        )
+
+        # User opens browser in login page
+        complement_url = reverse('authors:login_register')
+        self.browser.get(self.live_server_url + complement_url)
+
+        # User fill fields with username and password
+        form = self.get_by_fullxpath('/html/body/main/div[3]/form')
+        login = self.get_by_placeholder(form, 'Type your username here')
+        password = self.get_by_placeholder(form, 'Type your password here')
+        login.send_keys(user.username)
+        password.send_keys(passwd)
+
+        # User submit the form
+        form.submit()
+
+        # Form is reloaded
+        form = self.get_by_fullxpath('/html/body/main/div[3]/form')
+
+        # Testing...
+        self.assertIn(
+            'You are logged in as Username.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+        # User sees success flash message
+        # form = self.get_by_fullxpath('/html/body/main/div[3]/form')
