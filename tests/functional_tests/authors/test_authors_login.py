f56d9f15ba6e69c8addd62660d608f4a01f2352a
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-16 09:11:54 -0300

    Defining functional tests for unauthorized access to login_create view function (via get method) and invalid form login (login and password blank values)

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -32,9 +32,42 @@ class AuthorsLoginFunctionalTest(AuthorsBaseFunctionalTest):
 
         # Form is reloaded
         form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        self.sleep()
 
         # Testing...
         self.assertIn(
             f'You are logged in as {user.username}.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_login_create_raises_404_if_not_post_method(self):
+        # User trys access login page by get method
+        complement_url = reverse('authors:login_create')
+        self.browser.get(self.live_server_url + complement_url)
+        self.sleep()
+
+        # Testing if raises 404 error
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_login_view_if_user_is_not_authenticated_with_none_data(self):
+        # User opend login page
+        complement_url = reverse('authors:login_register')
+        self.browser.get(self.live_server_url + complement_url)
+        self.sleep()
+
+        # User types blank spaces in login and password fields
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        self.get_by_placeholder(form, 'Type your username here').send_keys('   ')  # noqa: E501
+        self.get_by_placeholder(form, 'Type your password here').send_keys('   ')  # noqa: E501
+
+        form.submit()
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        self.sleep()
+
+        self.assertIn(
+            'Invalid username or password',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
