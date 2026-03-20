e84846033a37111ec342c226a0f6ed8123ac5e8b
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-20 08:51:37 -0200

    Adjusting admin area for recipes model

--- recipes/admin.py
+++ recipes/admin.py
@@ -10,6 +10,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'is_published'
+    list_display = 'id', 'title', 'author', 'is_published'
     list_display_links = 'id',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    list_per_page = 8
+    list_filter = 'category', 'author', 'is_published', 'preparation_steps_is_html'  # noqa: E501
     search_fields = 'title', 'description', 'slug', 'preparation_steps'
