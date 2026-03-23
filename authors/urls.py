77a131c7cff7c8d89a13d806e63f6df7c2e037a0
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-23 15:13:20 -0300

    Starting to implement template, url and view function to edit recipe from dashboard

--- authors/urls.py
+++ authors/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('register/create/', views.register_create, name='register_create'),
     path('login/', views.login_view, name='login_register'),
     path('login/create/', views.login_create, name='login_create'),
-    path('logout', views.logout_view, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path(
+        'dashboard/recipe/<int:id>/edit/',
+        views.dashboard_recipe_edit,
+        name='dashboard_recipe_edit'
+    ),
 ]
