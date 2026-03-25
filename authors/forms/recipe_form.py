f4992377476cc0e0affcaf5ba05ed4e6debae1bf
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-24 23:51:03 -0300

    Updating CSS class in Recipe form fields

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,9 +1,23 @@
 from django import forms
 
 from recipes.models import Recipe
+from utils.django_forms import edit_attr_widget_field
 
 
 class AuthorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        edit_attr_widget_field(
+            self.fields.get('preparation_steps'),
+            'class',
+            'span-2'
+        )
+        edit_attr_widget_field(
+            self.fields.get('cover'),
+            'class',
+            'span-2'
+        )
+
     class Meta:
         model = Recipe
         fields = 'title', 'description', 'preparation_time', \
