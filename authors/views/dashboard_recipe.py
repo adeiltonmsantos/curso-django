83f2fb3f9f753fd90d2261a501534a3892df4496
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-04-03 17:18:21 -0300

    Using decorator to block acess of user without login in Class Based Views

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -1,12 +1,21 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
 
 from authors.forms import AuthorRecipeForm
 from recipes.models import Recipe
 
 
+@method_decorator(
+    login_required(
+        login_url='authors:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class DashboardRecipe(View):
     def get_recipe(self, id=None):
         recipe = None
