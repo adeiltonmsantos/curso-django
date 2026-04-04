f98bd87edbf4a4850a085702a704b719aa199d95
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-04-04 09:33:30 -0300

    Overwrinting save() method of Recipe model to define automatically slug field content

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User  # type: ignore
 from django.db import models  # type: ignore
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -38,3 +39,8 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse("recipes:recipe", args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify(self.title)}'
+        super().save(*args, **kwargs)
