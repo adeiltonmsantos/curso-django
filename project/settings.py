23de7172196c8241f066b958346134a710747428
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-07 14:56:02 -0300

    Updating .gitignore and settings.py (static folder for collectstatic)

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Where `collectstatic` will collect static files for production
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'base-static'
